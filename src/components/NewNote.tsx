bcdef0071bc25786ab5b87b97fdc7d79f8d66351
Reuben Ekow Yawson <87475624+Reuben-PropsDev@users.noreply.github.com>
2023-01-23 14:18:35 +0000

    refactor: refactor the title

--- src/components/NewNote.tsx
+++ src/components/NewNote.tsx
@@ -5,7 +5,7 @@ import NoteForm from './NoteForm'
 const NewNote = ({ onSubmit,onAddTag,availableTags}: NewNoteProps) => {
   return (
     <div className='my-4'>
-        <h1>NewNote</h1>
+        <h1>New Note</h1>
         <NoteForm onSubmit={onSubmit} onAddTag ={onAddTag} availableTags={availableTags} />
     </div>
   )
